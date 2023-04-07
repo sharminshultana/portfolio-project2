9041d1b8486b9198c37e8c64237daa320d758a73
sharminshultana <110155474+sharminshultana@users.noreply.github.com>
2023-04-06 22:37:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+// ---typing script js---
+var typed = new Typed('.typing', {
+    strings: ["web Designer", "web Developer", "Youtuber", "blogger"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+var typed = new Typed('.typing-2', {
+    strings: ["web Designer", "web Developer", "Youtuber", "blogger", "web Designer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+// -----show/hide faqs answer-----
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        // --change icon---
+        const icon = faq.querySelector('.faq-icon i');
+        if (icon.className === 'fa-solid fa-plus') {
+            icon.className = 'fa-solid fa-minus';
+        }
+        else {
+            icon.className = 'fa-solid fa-plus';
+        }
+
+    })
+})
+
+// ======show/hide nav menu=====
+
+const menu = document.querySelector('.nav-menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+
+
+// =======close nav menu=======
+const closeNav = () => {
+    menu.style.display = "none"
+    closeBtn.style.display = "none"
+    menuBtn.style.display = "inline-block"
+}
+closeBtn.addEventListener('click', closeNav);
+
+/* ====nav color change on click==== */
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100);
+})
